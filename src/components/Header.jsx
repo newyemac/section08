028a9ec57a0da6574c6192eccdc91dbcd9bf797e
ryusuik <newyemac@gmail.com>
2024-05-01 14:50:55 +0900

    5월1일 라면

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import "./Header.css"
 
 const Header = () => {
     return <div className="Header">
-        <h3>오늘은 🎰</h3> {/* windows키 + . 을 누르면 이모지가 나옴 */}
+        <h3>오늘은 📆</h3> {/* windows키 + . 을 누르면 이모지가 나옴 */}
         <h1>{new Date().toDateString()}</h1>
     </div>
 }
